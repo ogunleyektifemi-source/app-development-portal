adfe65b392ecc9d5fd697f4633fe89f79e83b995
APT-16 <aperson2116@gmail.com>
2026-07-28 01:11:41 +0100

    Remove temporary admin route

--- app.py
+++ app.py
@@ -1511,20 +1511,5 @@ def open_notification(notification_id):
 
     )
 
-@app.route("/create-first-admin-9F2KX7")
-def create_first_admin():
-
-    user = User.query.filter_by(
-        email="YOUR_NEW_EMAIL@example.com"
-    ).first()
-
-    if not user:
-        return "User not found."
-
-    user.is_admin = True
-    db.session.commit()
-
-    return "You are now an admin!"
-
 if __name__ == "__main__":
     app.run(debug=True)
