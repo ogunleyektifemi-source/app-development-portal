de215918dae7d4b2ce4e0e0dd41f2e30818bfe17
APT-16 <aperson2116@gmail.com>
2026-07-28 11:47:54 +0100

    Use PostgreSQL in production

--- config.py
+++ config.py
@@ -5,15 +5,30 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SECRET_KEY = "YOUR_SECRET_KEY_HERE"
-
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" +
-        os.path.join(
-            BASE_DIR,
-            "app_requests.db"
-        )
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "YOUR_SECRET_KEY_HERE"
     )
+
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if DATABASE_URL:
+
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    else:
+
+        SQLALCHEMY_DATABASE_URI = (
+            "sqlite:///" +
+            os.path.join(
+                BASE_DIR,
+                "app_requests.db"
+            )
+        )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -23,8 +38,8 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "YOUR_EMAIL@gmail.com"
-    MAIL_PASSWORD = "YOUR_APP_PASSWORD"
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
     # Uploads
@@ -38,7 +53,6 @@ class Config:
         UPLOAD_FOLDER,
         exist_ok=True
     )
-
 
     ALLOWED_EXTENSIONS = {
         "pdf",
