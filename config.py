376a0d03dfcbbe50a6efcf921efe8482a10faeb5
APT-16 <aperson2116@gmail.com>
2026-07-29 02:47:23 +0100

    Test Gmail SSL

--- config.py
+++ config.py
@@ -36,8 +36,9 @@ class Config:
     # Mail Configuration
 
     MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
